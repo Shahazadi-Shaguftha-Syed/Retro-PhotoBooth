bbd10b2fd4eb55023b69e116b538ed10bd55ebb5
Shahazadi-Shaguftha-Syed <shahazadishagufthasyed3@gmail.com>
2026-06-06 21:14:21 +0530

    updated

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 // export default defineConfig({ plugins: [react()] })
 export default defineConfig({
   plugins: [react()],
-  base: '/retro-photobooth/',  // must match your repo name
+  base: '/Retro-PhotoBooth/',  // must match your repo name
 })
